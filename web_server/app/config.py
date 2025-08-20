c535eb4759e7534c21403e1a7f9a89b60018e457
Dominik Schlösser <dominik.schloesser@gmail.com>
2025-08-20 15:58:27 +0200

    conf repr

--- web_server/app/config.py
+++ web_server/app/config.py
@@ -1,5 +1,6 @@
 import toml
 from pathlib import Path
+import os
 
 class Settings:
     def __init__(self, config_path: Path = Path(__file__).parent.parent / "web_server_config.toml"):
@@ -7,6 +8,8 @@ class Settings:
         self.server_host: str = config["server"]["host"]
         self.server_port: int = config["server"]["port"]
         self.resources_data_path: Path = (Path(__file__).parent.parent / config["resources"]["data_path"]).resolve()
+        # Must be in web-server stuff:
+        # self.resources_data_path: Path = os.path.expanduser(config["resources"]["data_path"])
         # self.tls_enabled: bool = config.get("tls", {}).get("enabled", False)
         # self.tls_keyfile: str | None = config.get("tls", {}).get("keyfile")
         # self.tls_certfile: str | None = config.get("tls", {}).get("certfile")
