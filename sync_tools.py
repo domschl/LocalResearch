e86eed9f4139758ac535fbd466c8b571a0952f8a
Dominik Schlösser <dominik.schloesser@gmail.com>
2025-11-16 09:19:48 +0100

    upd

--- sync_tools.py
+++ sync_tools.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import TypedDict
 
 
@@ -8,7 +9,23 @@ class SyncTarget(TypedDict):
     file_types: list[str]
 
 
+class FileMetadata(TypedDict):
+    path: str
+    date_changed: float
+    size: int
+    
 class SyncTools:
     def __init__(self, target:SyncTarget):
         self.log:logging.Logger = logging.Logger("SyncTool")
         self.target:SyncTarget = target
+        
+    def file_meta(self, filepath:str) -> tuple[float, int] | None:
+        try:
+            stat:os.stat_result = os.stat(filepath)
+            return  stat.st_mtime, stat.st_size
+        except Exception as e:
+            self.log.warning(f"Error getting file modification date {filepath}: {e}")
+        return None
+        
+    def update(self, rel_path:str):
+        pass
